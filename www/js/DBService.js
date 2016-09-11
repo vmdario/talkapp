2df4fb36af237cb4f91316e5e23f7dc98774df9c
Victor <victor.munidario@gmail.com>
2016-09-10 21:24:05 -0300

    db management complete

--- www/js/DBService.js
+++ www/js/DBService.js
@@ -10,7 +10,7 @@ angular.module('app.db', ['ionic', 'ngCordova'])
 		if (window.cordova) {
 			db = $cordovaSQLite.openDB({name: name, location: 'default'});
 		} else {
-			db = window.openDatabase(name, "", "", 200000);
+			db = window.openDatabase(name, "", "", 1024*1024*1);
 		}
 		return db;
 	};
@@ -60,70 +60,4 @@ angular.module('app.db', ['ionic', 'ngCordova'])
 		this.executeSQL(query, values, success_callback, err_callback);
 	};
 })
-
-.factory('MessagesFactory', function ($cordovaSQLite) {
-	return {
-		insert: function (contact, message, date) {
-
-			var query = "INSERT INTO messages (id, from_contact, to_contact, message, date) VALUES (?, ?, ?, ?);";
-			var values = [id, from_contact, to_contact, message, date];
-
-			$cordovaSQLite.execute(db, query, values).then(
-				function (res) {
-					console.log('INSERTED ID: ' + res.insertId);
-				},
-				function (err) {
-					console.log('ERROR: ' + err);
-				}
-			);
-		},
-		select: function (id) {
-
-			var query = "SELECT * FROM messages WHERE id=?";
-			var values = [id];
-
-			$cordovaSQLite.execute(db, query, values).then(function (res) {
-				if (res.rows.length > 0) {
-					var first = res.rows.item(0);
-					console.log(res.rows.length + ' records, fist: ' + first.firstname + ' ' + first.lastname + ' - ' + first.avatar);
-				} else {
-					console.log('No records found');
-				}
-			}
-			);
-		}
-	}
-})
-
-.factory('ContactsFactory', function ($cordovaSQLite) {
-	return {
-		insert: function (firstname, lastname, avatar, info) {
-
-			var query = "INSERT INTO contacts (firstname, lastname, avatar, info) VALUES (?, ?, ?, ?);";
-			var values = [firstname, lastname, avatar, info];
-
-			$cordovaSQLite.execute(db, query, values).then(
-				function (res) {
-					console.log('INSERTED ID: ' + res.insertId);
-				},
-				function (err) {
-					console.log('ERROR: ' + err);
-				}
-				);
-		},
-		select: function (id) {
-
-			var query = "SELECT * FROM contacts WHERE id=?";
-			var values = [id];
-
-			$cordovaSQLite.execute(db, query, values).then(function (res) {
-				if (res.rows.length > 0) {
-					var first = res.rows.item(0);
-					console.log(res.rows.length + ' records, fist: ' + first.firstname + ' ' + first.lastname + ' - ' + first.avatar);
-				} else {
-					console.log('No records found');
-				}
-			});
-		}
-	}
-});
+;
