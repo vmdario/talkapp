1add75a511818cac1d301f73f172a782292b67de
Victor Municelli Dario <victor.munidario@gmail.com>
2016-09-17 18:01:35 -0300

    Resource icons for android

--- www/js/app.js
+++ www/js/app.js
@@ -103,11 +103,11 @@ angular.module('app', [
 			console.log("Error in creating DB: "+err.message);
 		});
 
-		// DBService.insert("contacts", [1,'Carlos Ferreira','Available','555-55553']);
-		// DBService.insert("contacts", [2,'Julia Maria',':)','555-54253']);
-		// DBService.insert("contacts", [3,'Renata Martins','....','523-25553']);
+		DBService.insert("contacts", [1,'Carlos Ferreira','Available','555-55553']);
+		DBService.insert("contacts", [2,'Julia Maria',':)','555-54253']);
+		DBService.insert("contacts", [3,'Renata Martins','....','523-25553']);
 
-		// DBService.insert("messages", [1,1,2,'Testing ....',new Date('11/11/2015 12:53:05').toLocaleString()]);
-		// DBService.insert("messages", [2,1,3,'Another test',new Date('06/03/2015 17:33:45').toLocaleString()]);
+		DBService.insert("messages", [1,1,2,'Testing ....',new Date('11/11/2015 12:53:05').toLocaleString()]);
+		DBService.insert("messages", [2,1,3,'Another test',new Date('06/03/2015 17:33:45').toLocaleString()]);
 	});
 })
